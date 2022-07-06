9ca51c2f41d22cec2dbd63788b1ae0e432ac9b2c
NinNinNy <nearlymorning@gmail.com>
2022-07-07 02:06:07 +0700

    layout

--- app/src/main/java/stou/expproject/hiwapp/ReviewDetail.kt
+++ app/src/main/java/stou/expproject/hiwapp/ReviewDetail.kt
@@ -1,11 +1,24 @@
 package stou.expproject.hiwapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class ReviewDetail : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_review_detail)
+
+        val reviewList = Intent(this,ReviewList::class.java)
+        val loginPage = Intent(this,Login::class.java)
+
+        findViewById<Button>(R.id.btnLogin).setOnClickListener{
+            startActivity(loginPage)
+        }
+        findViewById<Button>(R.id.btnAllReview).setOnClickListener{
+            startActivity(reviewList)
+        }
+
     }
 }
